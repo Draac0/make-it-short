d376ed547cd116510bb67de65f29516dc0674756
Draac0 <asriram098@gmail.com>
2024-03-10 19:30:43 +0530

    init swagger & fastify

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter()
+  );
+
+  const config = new DocumentBuilder()
+      .setTitle('Make It Short')
+      .setDescription('Make It Short Api Documentation')
+      .setVersion('1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
-bootstrap();
+void bootstrap();
