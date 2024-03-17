9941244f23cf170f217552b3d7b7f5a96cdbdadf
Rahman <abdulrahman6093@outlook.com>
2024-03-17 20:10:27 +0530

    implemented variables for text and fixed lint issues in dropdown component

--- frontend/src/components/themeToggle.tsx
+++ frontend/src/components/themeToggle.tsx
@@ -10,6 +10,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import variables from "@/lib/variables";
 
 export function ThemeToggle(): React.ReactElement {
   const { setTheme } = useTheme();
@@ -20,7 +21,7 @@ export function ThemeToggle(): React.ReactElement {
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">{variables.en.toggle_theme}</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
@@ -29,21 +30,21 @@ export function ThemeToggle(): React.ReactElement {
             setTheme("light");
           }}
         >
-          Light
+          {variables.en.light}
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => {
             setTheme("dark");
           }}
         >
-          Dark
+          {variables.en.dark}
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => {
             setTheme("system");
           }}
         >
-          System
+          {variables.en.system}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
--- frontend/src/containers/navbar/index.tsx
+++ frontend/src/containers/navbar/index.tsx
@@ -1,10 +1,11 @@
 import { type ReactElement } from "react";
 import { ThemeToggle } from "@/components/themeToggle";
+import variables from "@/lib/variables";
 
 const Navbar = (): ReactElement => {
   return (
     <header className="flex justify-between p-10 absolute w-full">
-      <p className="text-2xl font-semibold">Make it short</p>
+      <p className="text-2xl font-semibold">{variables.en.make_it_short}</p>
       <ThemeToggle />
     </header>
   );
