b2e9c62d972e64ef7315e50a081d61544a3d1815
Draac0 <asriram098@gmail.com>
2024-03-17 10:49:34 +0530

    url entity defined

--- backend/src/modules/users/user.entity.ts
+++ backend/src/modules/users/user.entity.ts
@@ -1,22 +1,26 @@
 import { BaseEntity } from '../../common/base-entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
+import { UrlEntity } from '@/modules/url/url.entity';
 
 @Entity({ name: 'users' })
 export class UserEntity extends BaseEntity {
-  @Column({ name: 'first_name', nullable: false })
-  @ApiProperty()
-  firstName: string;
+	@Column({ name: 'first_name', nullable: false })
+	@ApiProperty()
+	firstName: string;
 
-  @Column({ name: 'last_name', nullable: false })
-  @ApiProperty()
-  lastName: string;
+	@Column({ name: 'last_name', nullable: false })
+	@ApiProperty()
+	lastName: string;
 
-  @Column({ name: 'email', nullable: false })
-  @ApiProperty()
-  email: string;
+	@Column({ name: 'email', nullable: false })
+	@ApiProperty()
+	email: string;
 
-  @Column({ name: 'password', nullable: false })
-  @ApiProperty()
-  password: string;
+	@Column({ name: 'password', nullable: false })
+	@ApiProperty()
+	password: string;
+
+	@OneToMany(() => UrlEntity, (url) => url.user)
+	urls: UrlEntity[];
 }
