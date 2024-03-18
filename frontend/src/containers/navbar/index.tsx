6dc1fb3a5ba9a0a59cfe626f5f461df1102e8eca
Rahman <abdulrahman6093@outlook.com>
2024-03-18 13:28:45 +0530

    added .idea to gitignore

--- frontend/src/containers/navbar/index.tsx
+++ frontend/src/containers/navbar/index.tsx
@@ -4,7 +4,7 @@ import variables from "@/lib/variables";
 
 const Navbar = (): ReactElement => {
   return (
-    <header className="flex justify-between p-10 absolute w-full">
+    <header className="flex justify-between items-center py-6 px-10 absolute w-full border-b border-gray-200 dark:border-gray-800">
       <p className="text-2xl font-semibold">{variables.en.make_it_short}</p>
       <ThemeToggle />
     </header>
